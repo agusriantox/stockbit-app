0179565d274bfcede74bedcbe57590f3cc041331
Agus Rianto <>
2021-01-04 10:20:01 +0700

    handle user need login or not

--- app/src/main/java/com/stockbit/app/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/stockbit/app/ui/splashscreen/SplashScreen.kt
@@ -1,12 +1,28 @@
 package com.stockbit.app.ui.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.stockbit.app.R
+import com.stockbit.app.data.local.AuthRepository
+import com.stockbit.app.ui.login.LoginActivity
+import com.stockbit.app.ui.main.MainActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (AuthRepository.isLogin()){
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+            finish()
+        }, 500)
+
     }
 }
