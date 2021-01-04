0ae50fa000c4291e916dbd30b523a8e977c54775
Agus Rianto <>
2021-01-04 10:18:35 +0700

    add splash screen page

---
+++ app/src/main/java/com/stockbit/app/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.stockbit.app.ui.splashscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stockbit.app.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
